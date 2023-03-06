a8a135bffe56a8d38db44564099d518d2ec16127
enoatu <acro1452@gmail.com>
2023-03-06 23:16:27 +0900

    :wrench: modify

--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 ## MEMO
 ```
 flutter create -i swift -a kotlin flutter_coin_trade_supporter
-flutter pub add flutter_local_notifications
+flutter pub add flutter_local_notifications flutter_background_service
+
+open -a Simulator
+~/Library/Android/sdk/emulator/emulator -avd Pixel_3_API_30
 ```
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,83 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+// ref
+// https://zenn.dev/tomon9086/articles/d2624f6ab37c4c
+// https://take424.dev/2021/05/22/flutter%E3%81%A7%E3%83%AD%E3%83%BC%E3%82%AB%E3%83%AB%E9%80%9A%E7%9F%A5%E3%81%AE%E5%8B%95%E4%BD%9C%E3%82%92%E7%A2%BA%E8%AA%8D%E3%81%99%E3%82%8B%EF%BC%8Fflutter_local_notifications/
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+// ref
+// https://pub.dev/packages/flutter_background_service
+// https://smt-create.com/?p=346
+import 'package:flutter_background_service/flutter_background_service.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initializeService();
   runApp(const MyApp());
+}
+
+final service = FlutterBackgroundService();
+
+// loop
+Future<void> onStart(ServiceInstance service) async {
+  //service.on('stopService').listen((event) {
+  //  service.stopSelf();
+  //});
+  service.on('startService').listen((event) {
+    Timer.periodic(const Duration(seconds: 15), (timer) async {
+      await showNotification();
+    });
+  });
+}
+
+Future<void> initializeService() async {
+  await service.configure(
+    androidConfiguration: AndroidConfiguration(
+      // this will be executed when app is in foreground or background in separated isolate
+      onStart: onStart,
+
+      // auto start service
+      autoStart: true,
+      isForegroundMode: true,
+    ),
+    iosConfiguration: IosConfiguration(
+      // this will be executed when app is in foreground or background in separated isolate
+      // onStart: onStart,
+    ),
+  );
+}
+// Notice
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+Future<void> showNotification() async {
+  var androidChannelSpecifics = const AndroidNotificationDetails(
+    'CHANNEL_ID',
+    'CHANNEL_NAME',
+    channelDescription: "CHANNEL_DESCRIPTION",
+    largeIcon: DrawableResourceAndroidBitmap('ic_stat_name'),
+    icon: 'ic_stat_name',
+    importance: Importance.max,
+    priority: Priority.high,
+    playSound: false,
+    timeoutAfter: 5000,
+    styleInformation: DefaultStyleInformation(
+      true,
+      true,
+    ),
+  );
+
+  var iosChannelSpecifics = const DarwinNotificationDetails();
+
+  var platformChannelSpecifics = NotificationDetails(
+    android: androidChannelSpecifics,
+    iOS: iosChannelSpecifics,
+  );
+
+  await flutterLocalNotificationsPlugin.show(
+    1, // Notification ID
+    'Test Title', // Notification Title
+    'Test Body', // Notification Body, set as null to remove the body
+    platformChannelSpecifics,
+    payload: 'New Payload', // Notification Payload
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -31,44 +106,11 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
 
   void _incrementCounter() {
     setState(() {
       _counter++;
     });
-  }
-  Future<void> _showNotification() async {
-    var androidChannelSpecifics = const AndroidNotificationDetails(
-      'CHANNEL_ID',
-      'CHANNEL_NAME',
-      channelDescription: "CHANNEL_DESCRIPTION",
-      largeIcon: DrawableResourceAndroidBitmap('ic_stat_name'),
-      icon: 'ic_stat_name',
-      importance: Importance.max,
-      priority: Priority.high,
-      playSound: false,
-      timeoutAfter: 5000,
-      styleInformation: DefaultStyleInformation(
-        true,
-        true,
-      ),
-    );
-
-    var iosChannelSpecifics = const DarwinNotificationDetails();
-
-    var platformChannelSpecifics = NotificationDetails(
-      android: androidChannelSpecifics,
-      iOS: iosChannelSpecifics,
-    );
-
-    await flutterLocalNotificationsPlugin.show(
-      1, // Notification ID
-      'Test Title', // Notification Title
-      'Test Body', // Notification Body, set as null to remove the body
-      platformChannelSpecifics,
-      payload: 'New Payload', // Notification Payload
-    );
   }
 
   @override
@@ -83,7 +125,7 @@ class _MyHomePageState extends State<MyHomePage> {
           children: <Widget>[
             ElevatedButton(
               onPressed: () {
-                _showNotification(); // 通知をすぐに表示
+                showNotification(); // 通知をすぐに表示
               },
               child: const Text('すぐに通知を表示'),
             ),
@@ -94,6 +136,16 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
             ),
+            ElevatedButton(
+                child: const Text("Start Notication"),
+                onPressed: () {
+                  service.invoke('startService');
+                }),
+            ElevatedButton(
+                child: const Text("Stop Notication"),
+                onPressed: () {
+                  service.invoke("stopService");
+                }),
           ],
         ),
       ),
